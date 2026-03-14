192458e9850dbeebaa0f393bf214165d7f78498b
huntd <huntd@example.com>
2026-03-14 07:17:10 -0500

    Fix next.config.js: Convert to CommonJS syntax to resolve build error

--- next.config.js
+++ next.config.js
@@ -16,20 +16,3 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-      },
-    ],
-  },
-};
-
-module.exports = nextConfig;
